0ba55da4ef5bc740b540bf39dc2e96e9e785cc16
mohammedAlefrangy <mohamed.alefrangy@gmail.com>
2018-12-02 23:09:54 +0200

    1- import another class about search button

--- app/src/main/java/com/example/hmod_/animeapp/MainActivity.java
+++ app/src/main/java/com/example/hmod_/animeapp/MainActivity.java
@@ -25,13 +25,14 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
+
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
